75e6b55b6f457756deb2017b1360637cf3ccef9a
Luis Sierra <37555594+LGSierra@users.noreply.github.com>
2022-02-08 04:40:12 -0400

    Finalized required steps

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -63,7 +64,15 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageurl;
+
+            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                imageurl = movie.getBackdropPath();
+            } else{
+                imageurl = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageurl).into(ivPoster);
         }
     }
 }
